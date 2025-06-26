356f1784e2dd3478f9bc436ba5c73862afdad19e
eminamumic <emimumic1@gmail.com>
2025-06-26 16:21:24 +0200

    Add swagger api

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Location API')
+    .setDescription(
+      'The API for managing locations in the MiniStock application.',
+    )
+    .setVersion('1.0')
+    .addTag('locations', 'Operations related to locations')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
+  console.log(`Swagger UI available at: ${await app.getUrl()}/api`);
 }
 bootstrap();
