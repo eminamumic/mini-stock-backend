224b92aafa38eebc5fb070fe35137706f1d1188c
eminamumic <emimumic1@gmail.com>
2025-06-27 21:52:49 +0200

    Add searchCategories method in category service

--- src/modules/category/category.service.ts
+++ src/modules/category/category.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Category } from 'src/entities/category/category';
 import { CreateCategoryDto } from './dto/create-category.dto';
+import { SearchCategoryDto } from './dto/search-category.dto';
+import { FindOptionsWhere } from 'typeorm';
+import { Like } from 'typeorm';
 
 import { Repository } from 'typeorm';
 
@@ -25,5 +28,25 @@ export class CategoryService {
 
   async getCategoryById(id: number): Promise<Category | null> {
     return this.categoryRepository.findOne({ where: { id } });
+  }
+
+  async searchCategories(
+    searchCriteria: SearchCategoryDto,
+  ): Promise<Category[]> {
+    const whereClause: FindOptionsWhere<Category> = {};
+
+    if (searchCriteria.id) {
+      whereClause.id = searchCriteria.id;
+    }
+    if (searchCriteria.name) {
+      whereClause.name = Like(`%${searchCriteria.name}%`);
+    }
+    if (searchCriteria.description) {
+      whereClause.description = Like(`%${searchCriteria.description}%`);
+    }
+
+    return this.categoryRepository.find({
+      where: whereClause,
+    });
   }
 }
