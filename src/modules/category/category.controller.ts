4b3917fb206472c21ae9dc66c8b5f7ad95b94a93
eminamumic <emimumic1@gmail.com>
2025-06-27 22:06:28 +0200

    Add create method in category controller

---
+++ src/modules/category/category.controller.ts
@@ -0,0 +1,27 @@
+import {
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UsePipes,
+  ValidationPipe,
+  Body,
+} from '@nestjs/common';
+
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { Category } from 'src/entities/category/category';
+import { CategoryService } from './category.service';
+
+@Controller('categories')
+export class CategoryController {
+  constructor(private readonly categoryService: CategoryService) {}
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  @UsePipes(new ValidationPipe({ whitelist: true, transform: true }))
+  async create(
+    @Body() createCategoryDto: CreateCategoryDto,
+  ): Promise<Category> {
+    return this.categoryService.createCategory(createCategoryDto);
+  }
+}
