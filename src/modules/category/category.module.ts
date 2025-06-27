9f2545eb8030360251b16c8a873f50911f4021c4
eminamumic <emimumic1@gmail.com>
2025-06-27 22:27:41 +0200

    Add category module

---
+++ src/modules/category/category.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoryService } from './category.service';
+import { CategoryController } from './category.controller';
+import { Category } from 'src/entities/category/category';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Category])],
+  controllers: [CategoryController],
+  providers: [CategoryService],
+  exports: [CategoryService],
+})
+export class CategoryModule {}
