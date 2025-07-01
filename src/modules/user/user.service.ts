4076378a152bbc0ea8f75c715518c40cb898ca2b
eminamumic <emimumic1@gmail.com>
2025-07-01 14:51:13 +0200

    Add createUser method in user service

---
+++ src/modules/user/user.service.ts
@@ -0,0 +1,47 @@
+import { Injectable, ConflictException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository, Like, FindOptionsWhere } from 'typeorm';
+import { User } from 'src/entities/user/user';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { SearchUserDto } from './dto/search-user.dto';
+import * as bcrypt from 'bcryptjs';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async createUser(createUserDto: CreateUserDto): Promise<User> {
+    const existingUserByUsername = await this.userRepository.findOne({
+      where: { username: createUserDto.username },
+    });
+    if (existingUserByUsername) {
+      throw new ConflictException('Username already exists.');
+    }
+
+    if (createUserDto.email) {
+      const existingUserByEmail = await this.userRepository.findOne({
+        where: { email: createUserDto.email },
+      });
+      if (existingUserByEmail) {
+        throw new ConflictException('Email already exists.');
+      }
+    }
+
+    const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
+
+    const { password, ...userDataWithoutPassword } = createUserDto;
+
+    const newUser = this.userRepository.create({
+      ...userDataWithoutPassword,
+      hashedPassword,
+      createdAt: new Date(),
+      isActive: true,
+    });
+
+    return this.userRepository.save(newUser);
+  }
+}
