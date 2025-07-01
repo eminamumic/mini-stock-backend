6c40de1f711348aaa33ccb71714ef22fcb25fabd
eminamumic <emimumic1@gmail.com>
2025-07-01 14:54:35 +0200

    Add updateUser and deleteUser method in user service

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -86,4 +86,52 @@ export class UserService {
       where: whereClause,
     });
   }
+
+  async updateUser(
+    id: number,
+    updateUserDto: UpdateUserDto,
+  ): Promise<User | null> {
+    const userToUpdate = await this.userRepository.findOne({ where: { id } });
+
+    if (!userToUpdate) {
+      return null;
+    }
+
+    if (
+      updateUserDto.username &&
+      updateUserDto.username !== userToUpdate.username
+    ) {
+      const existingUserByUsername = await this.userRepository.findOne({
+        where: { username: updateUserDto.username },
+      });
+      if (existingUserByUsername && existingUserByUsername.id !== id) {
+        throw new ConflictException('Username already exists.');
+      }
+    }
+
+    if (updateUserDto.email && updateUserDto.email !== userToUpdate.email) {
+      const existingUserByEmail = await this.userRepository.findOne({
+        where: { email: updateUserDto.email },
+      });
+      if (existingUserByEmail && existingUserByEmail.id !== id) {
+        throw new ConflictException('Email already exists.');
+      }
+    }
+
+    const dataToMerge: Partial<User> = { ...updateUserDto };
+
+    if (updateUserDto.password) {
+      const newHashedPassword = await bcrypt.hash(updateUserDto.password, 10);
+      userToUpdate.hashedPassword = newHashedPassword;
+      delete dataToMerge.hashedPassword;
+    }
+
+    this.userRepository.merge(userToUpdate, dataToMerge);
+    return this.userRepository.save(userToUpdate);
+  }
+
+  async deleteUser(id: number): Promise<boolean> {
+    const deleteResult = await this.userRepository.delete(id);
+    return deleteResult.affected !== 0;
+  }
 }
