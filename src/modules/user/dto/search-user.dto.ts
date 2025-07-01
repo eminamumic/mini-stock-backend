508b06ea52d5d3d957bf39ab5808a6676afa8c43
eminamumic <emimumic1@gmail.com>
2025-07-01 13:44:28 +0200

    Add dto for searching user

---
+++ src/modules/user/dto/search-user.dto.ts
@@ -0,0 +1,72 @@
+import {
+  IsString,
+  IsOptional,
+  IsNumberString,
+  IsBooleanString,
+} from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class SearchUserDto {
+  @ApiProperty({
+    description: 'The ID of the user for searching',
+    example: '1',
+    required: false,
+  })
+  @IsOptional()
+  @IsNumberString()
+  id?: number;
+
+  @ApiProperty({
+    description: 'Username for searching',
+    example: 'emina.mumic',
+    required: false,
+  })
+  @IsOptional()
+  @IsString()
+  username?: string;
+
+  @ApiProperty({
+    description: 'First name for searching',
+    example: 'Emina',
+    required: false,
+  })
+  @IsOptional()
+  @IsString()
+  firstName?: string;
+
+  @ApiProperty({
+    description: 'Last name for searching (partial match)',
+    example: 'Mumic',
+    required: false,
+  })
+  @IsOptional()
+  @IsString()
+  lastName?: string;
+
+  @ApiProperty({
+    description: 'Email address for searching',
+    example: 'emina@gmail.com',
+    required: false,
+  })
+  @IsOptional()
+  @IsString()
+  email?: string;
+
+  @ApiProperty({
+    description: 'Is the user active for searching (true/false)',
+    example: 'true',
+    required: false,
+  })
+  @IsOptional()
+  @IsBooleanString() // Za boolean query parametre
+  isActive?: boolean;
+
+  @ApiProperty({
+    description: 'User role for searching',
+    example: 'employee',
+    required: false,
+  })
+  @IsOptional()
+  @IsString()
+  userRole?: string;
+}
