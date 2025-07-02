2a36ccf6162f23086eed50c7144400cf7e960980
eminamumic <emimumic1@gmail.com>
2025-07-02 14:19:27 +0200

    Add search dto for warehouse

---
+++ src/modules/warehouse/dto/search-warehouse.dto.ts
@@ -0,0 +1,66 @@
+import {
+  IsString,
+  IsOptional,
+  IsNumberString,
+  IsBooleanString,
+  IsDateString,
+} from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class SearchWarehouseDto {
+  @ApiProperty({
+    description: 'The ID of the warehouse for searching',
+    example: '1',
+    required: false,
+  })
+  @IsOptional()
+  @IsNumberString()
+  id?: number;
+
+  @ApiProperty({
+    description: 'Name of the warehouse for searching ',
+    example: 'Main',
+    required: false,
+  })
+  @IsOptional()
+  @IsString()
+  name?: string;
+
+  @ApiProperty({
+    description: 'Location ID for searching',
+    example: '1',
+    required: false,
+  })
+  @IsOptional()
+  @IsNumberString()
+  locationId?: number;
+
+  @ApiProperty({
+    description: 'Warehouse Type ID for searching',
+    example: '1',
+    required: false,
+  })
+  @IsOptional()
+  @IsNumberString()
+  warehouseTypeId?: number;
+
+  @ApiProperty({
+    description: 'Creation date for searching (YYYY-MM-DD format).',
+    example: '2024-01-01',
+    required: false,
+    type: 'string',
+    format: 'date',
+  })
+  @IsOptional()
+  @IsDateString()
+  createdAt?: Date;
+
+  @ApiProperty({
+    description: 'Is the warehouse active for searching (true/false)',
+    example: 'true',
+    required: false,
+  })
+  @IsOptional()
+  @IsBooleanString()
+  isActive?: boolean;
+}
