2d3958e132d6c57be33aab2ff0d930383a5df650
eminamumic <emimumic1@gmail.com>
2025-07-03 15:21:51 +0200

    Add create method in stock-level service

---
+++ src/modules/stock-level/stock-level.service.ts
@@ -0,0 +1,63 @@
+import {
+  Injectable,
+  ConflictException,
+  BadRequestException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository, FindOptionsWhere } from 'typeorm';
+import { StockLevel } from 'src/entities/stock-level/stock-level';
+import { Product } from 'src/entities/product/product';
+import { Warehouse } from 'src/entities/warehouse/warehouse';
+import { CreateStockLevelDto } from './dto/create-stock-level.dto';
+import { UpdateStockLevelDto } from './dto/update-stock-level.dto';
+import { SearchStockLevelDto } from './dto/search-stock-level.dto';
+
+@Injectable()
+export class StockLevelService {
+  constructor(
+    @InjectRepository(StockLevel)
+    private readonly stockLevelRepository: Repository<StockLevel>,
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+    @InjectRepository(Warehouse)
+    private readonly warehouseRepository: Repository<Warehouse>,
+  ) {}
+
+  async create(createStockLevelDto: CreateStockLevelDto): Promise<StockLevel> {
+    const existingProduct = await this.productRepository.findOne({
+      where: { id: createStockLevelDto.productId },
+    });
+    if (!existingProduct) {
+      throw new BadRequestException(
+        `Product with ID ${createStockLevelDto.productId} not found.`,
+      );
+    }
+
+    const existingWarehouse = await this.warehouseRepository.findOne({
+      where: { id: createStockLevelDto.warehouseId },
+    });
+    if (!existingWarehouse) {
+      throw new BadRequestException(
+        `Warehouse with ID ${createStockLevelDto.warehouseId} not found.`,
+      );
+    }
+
+    const existingStockLevel = await this.stockLevelRepository.findOne({
+      where: {
+        productId: createStockLevelDto.productId,
+        warehouseId: createStockLevelDto.warehouseId,
+      },
+    });
+    if (existingStockLevel) {
+      throw new ConflictException(
+        `Stock level for Product ID ${createStockLevelDto.productId} in Warehouse ID ${createStockLevelDto.warehouseId} already exists.`,
+      );
+    }
+
+    const newStockLevel = this.stockLevelRepository.create({
+      ...createStockLevelDto,
+    });
+
+    return this.stockLevelRepository.save(newStockLevel);
+  }
+}
