8fc7f5295905efec84bf94cf41b0919d0b2b6856
eminamumic <emimumic1@gmail.com>
2025-07-15 11:16:58 +0200

    Add update method in warehouse access service

--- src/modules/warehouse-access/warehouse-access.service.ts
+++ src/modules/warehouse-access/warehouse-access.service.ts
@@ -99,4 +99,82 @@ export class WarehouseAccessService {
       relations: ['employee', 'warehouse'],
     });
   }
+
+  async update(
+    id: number,
+    updateWarehouseAccessDto: UpdateWarehouseAccessDto,
+  ): Promise<WarehouseAccess | null> {
+    const warehouseAccessToUpdate =
+      await this.warehouseAccessRepository.findOne({
+        where: { id },
+      });
+
+    if (!warehouseAccessToUpdate) {
+      return null;
+    }
+
+    if (
+      updateWarehouseAccessDto.employeeId !== undefined &&
+      updateWarehouseAccessDto.employeeId !== warehouseAccessToUpdate.employeeId
+    ) {
+      const existingEmployee = await this.employeeRepository.findOne({
+        where: { id: updateWarehouseAccessDto.employeeId },
+      });
+      if (!existingEmployee) {
+        throw new BadRequestException(
+          `Zaposlenik s ID-em ${updateWarehouseAccessDto.employeeId} nije pronađen.`,
+        );
+      }
+    }
+
+    if (
+      updateWarehouseAccessDto.warehouseId !== undefined &&
+      updateWarehouseAccessDto.warehouseId !==
+        warehouseAccessToUpdate.warehouseId
+    ) {
+      const existingWarehouse = await this.warehouseRepository.findOne({
+        where: { id: updateWarehouseAccessDto.warehouseId },
+      });
+      if (!existingWarehouse) {
+        throw new BadRequestException(
+          `Skladište s ID-em ${updateWarehouseAccessDto.warehouseId} nije pronađeno.`,
+        );
+      }
+    }
+
+    if (
+      (updateWarehouseAccessDto.employeeId !== undefined &&
+        updateWarehouseAccessDto.employeeId !==
+          warehouseAccessToUpdate.employeeId) ||
+      (updateWarehouseAccessDto.warehouseId !== undefined &&
+        updateWarehouseAccessDto.warehouseId !==
+          warehouseAccessToUpdate.warehouseId)
+    ) {
+      const targetEmployeeId =
+        updateWarehouseAccessDto.employeeId ||
+        warehouseAccessToUpdate.employeeId;
+      const targetWarehouseId =
+        updateWarehouseAccessDto.warehouseId ||
+        warehouseAccessToUpdate.warehouseId;
+
+      const conflictingAccess = await this.warehouseAccessRepository.findOne({
+        where: {
+          employeeId: targetEmployeeId,
+          warehouseId: targetWarehouseId,
+        },
+      });
+
+      if (conflictingAccess && conflictingAccess.id !== id) {
+        throw new ConflictException(
+          `Pristup za Zaposlenika ID ${targetEmployeeId} u Skladištu ID ${targetWarehouseId} već postoji.`,
+        );
+      }
+    }
+
+    this.warehouseAccessRepository.merge(warehouseAccessToUpdate, {
+      ...updateWarehouseAccessDto,
+    });
+
+    return this.warehouseAccessRepository.save(warehouseAccessToUpdate);
+  }
 }
