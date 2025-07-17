33cd6a020342127ece28a3a6e4d4ed1e7f68e426
eminamumic <emimumic1@gmail.com>
2025-07-17 12:19:58 +0200

    Add user repositry to employee module

--- src/modules/employee/employee.module.ts
+++ src/modules/employee/employee.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Employee } from 'src/entities/employee/employee';
 import { EmployeeController } from './employee.controller';
 import { EmployeeService } from './employee.service';
+import { User } from 'src/entities/user/user';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Employee])],
+  imports: [TypeOrmModule.forFeature([Employee, User])],
   controllers: [EmployeeController],
   providers: [EmployeeService],
   exports: [EmployeeService],
