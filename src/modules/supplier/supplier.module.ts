b1750c9cd0ed99be7e721c0600db8de296dcc5a5
eminamumic <emimumic1@gmail.com>
2025-07-17 12:21:25 +0200

    Add location repository to supplier module

--- src/modules/supplier/supplier.module.ts
+++ src/modules/supplier/supplier.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Supplier } from 'src/entities/supplier/supplier';
 import { SupplierController } from './supplier.controller';
 import { SupplierService } from './supplier.service';
+import { Location } from 'src/entities/location/location';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Supplier])],
+  imports: [TypeOrmModule.forFeature([Supplier, Location])],
   controllers: [SupplierController],
   providers: [SupplierService],
   exports: [SupplierService],
