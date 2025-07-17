85d3230623c77088c8eab5ef0f5182394d1cb710
eminamumic <emimumic1@gmail.com>
2025-07-17 12:20:50 +0200

    Add product and warehouse repository to stock-level module

--- src/modules/stock-level/stock-level.module.ts
+++ src/modules/stock-level/stock-level.module.ts
@@ -3,9 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { StockLevel } from 'src/entities/stock-level/stock-level';
 import { StockLevelController } from './stock-level.sontroller';
 import { StockLevelService } from './stock-level.service';
+import { Product } from 'src/entities/product/product';
+import { Warehouse } from 'src/entities/warehouse/warehouse';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([StockLevel])],
+  imports: [TypeOrmModule.forFeature([StockLevel, Product, Warehouse])],
   controllers: [StockLevelController],
   providers: [StockLevelService],
   exports: [StockLevelService],
