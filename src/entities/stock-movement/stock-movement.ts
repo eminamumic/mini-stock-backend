c2714f287643fd3cb5aee8cbd152c4645032910d
eminamumic <emimumic1@gmail.com>
2025-08-21 16:57:25 +0200

    Add createAt i updateAd column in stock movement entity

--- src/entities/stock-movement/stock-movement.ts
+++ src/entities/stock-movement/stock-movement.ts
@@ -91,4 +91,19 @@ export class StockMovement {
 
   @Column({ type: 'text', nullable: true })
   note: string;
+
+  @Column({
+    name: 'created_at',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createdAt: Date;
+
+  @Column({
+    name: 'updated_at',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+  })
+  updatedAt: Date;
 }
